7074f7058e1acef08bdd4991989d7c82182bef0e
svetlaya1 <svetlaya_st@list.ru>
2022-03-02 00:58:55 +0800

    add changes

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
 import 'normalize.css'
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import './styles/main.scss'
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+var swiper = new Swiper(".offerSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    modules: [Navigation, Pagination],
+  });
