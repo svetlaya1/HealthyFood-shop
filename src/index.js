64fb94b76192cb93160454ed24e9dfff3851c0cd
svetlaya1 <svetlaya_st@list.ru>
2022-03-03 18:09:53 +0800

    add new block

--- src/index.js
+++ src/index.js
@@ -22,3 +22,20 @@ var swiper = new Swiper(".offerSwiper", {
     },
     modules: [Navigation, Pagination],
   });
+
+  var swiper = new Swiper(".trendsSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    modules: [Navigation, Pagination],
+  });
